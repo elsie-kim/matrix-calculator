4ba140675b956dea381ff7d3e88c0ac52c4ce30c
k1614 <elsiekim24@gmail.com>
2020-08-04 15:26:50 -0400

    Fixed rounding issues

--- calc-c++/Calculator.cpp
+++ calc-c++/Calculator.cpp
@@ -6,9 +6,35 @@ std::string Calculator::int_or_float(float x) {
 	if (x == int(x)) {
 		int y = int(x);
 		return std::to_string(y);
+
 	} else {
-		return std::to_string(x);
+		return round_val(std::to_string(x));
 	}
+}
+
+std::string Calculator::concatenate(std::string x) {
+	std::string::size_type index = x.find('.');
+
+	for (int i=x.length()-1; i>index; i--) {
+		if (x[i] == '0' && x[i-1] != '0') {
+			return x.substr(0, i);
+		}
+	}
+
+	return x;
+}
+
+std::string Calculator::round_val(std::string x) {
+	float y = std::stof(x);
+	y *= 10000;
+	y = round(y);
+
+	if (std::to_string(y).find('.') != std::string::npos) {
+		return concatenate(std::to_string(y/10000));
+	} 
+
+	return std::to_string(y/10000);
+	
 }
 
 float Calculator::dot_product(std::vector<float>& v1, std::vector<float>& v2)
@@ -19,7 +45,14 @@ float Calculator::dot_product(std::vector<float>& v1, std::vector<float>& v2)
 	{
 		sum += v1[i] * v2[i];
 	}
-    
+   
+	std::string s_str = std::to_string(sum);
+
+	if (sum != int(sum) && s_str.rfind('0') == (s_str.size()-1)) {		
+		std::string sum_string = std::to_string(sum);
+		sum_string = round_val(sum_string);
+		sum = std::stof(sum_string);
+	}	
 	return sum;
 }
 
@@ -32,7 +65,7 @@ std::string Calculator::cross_product(std::vector<float>&v1, std::vector<float>&
 	float c = v1[0]*v2[1] - v1[1]*v2[0];
 
 	ans = "(" + int_or_float(a) + ", " + int_or_float(b) + ", " + int_or_float(c) + ")";
-	
+
 	return ans;
 }
 
--- calc-c++/Calculator.h
+++ calc-c++/Calculator.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <math.h>
 
 // Calculator object contains vector operation methods (such as dot product)
 
@@ -11,7 +12,9 @@ class Calculator
 {
     public:
         std::vector<std::string> operations = {"dot_product"};
-	std::string int_or_float(float x);   
+	std::string int_or_float(float x);  
+	std::string concatenate(std::string x);
+	std::string round_val(std::string x);
 	float dot_product(std::vector<float>& v, std::vector<float>& v2);
 	std::string cross_product(std::vector<float>&v, std::vector<float>& v2); 
 
